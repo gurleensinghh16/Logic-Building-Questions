691d9ca94fe8e896913504d07494fb2d49c9a0df
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-01 01:42:58 +0530

    Recursion Question

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
+int fac=1;
+void fact(int n){
+    if(n==1) return ;
+    fac*=n;
+    fact(n-1);
+}
 int main ()
 {
-    
+    int n;
+    cout<<"Enter value of n :";
+    cin>>n;
+    fact(n);
+    cout<<"Factorial is : "<<fac<<endl;
     return 0;
 }
