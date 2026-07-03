c47bd38c862435404784e960fb2904fd08b687c7
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-03 23:27:42 +0530

    Recursion Question

---
+++ Recursion/print_array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void print(int arr[],int n,int i=0){
+ if(i==n) return;
+ cout<<arr[i]<<" ";
+ print(arr,n,i+=1);
+}
+int main ()
+{
+    int n;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements inside tha array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    print(arr,n);
+    cout<<endl;
+    return 0;
+}
