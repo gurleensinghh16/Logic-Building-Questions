282fcba9d3265e9fa273d52fc8c2dfddeb24d2cd
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-03 23:50:43 +0530

    Recursion Question

---
+++ Recursion/Sum_Array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void print(int arr[],int n,int i=0,int sum=0){
+ if(i==n){
+    cout<<sum;
+    return ;
+ } 
+ print(arr,n,i+1,sum+=arr[i]);
+}
+int main ()
+{
+    int n;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements inside tha array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    print(arr,n);
+    cout<<endl;
+    return 0;
+}
