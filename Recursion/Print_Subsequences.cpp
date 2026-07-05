eb26507d13c5e87caf77dccde6b6ddbde25bb904
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-05 22:17:43 +0530

    Recursion Question

---
+++ Recursion/Print_Subsequences.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void subsequence(int arr[], int n, int index, vector<int> &current)
+{
+    if(index == n)
+    {
+        for(int x : current)
+            cout << x << " ";
+        cout << endl;
+        return;
+    }
+
+    // Choice 1 : Take the current element
+    current.push_back(arr[index]);
+    subsequence(arr, n, index + 1, current);
+
+    // Backtrack
+    current.pop_back();
+
+    // Choice 2 : Skip the current element
+    subsequence(arr, n, index + 1, current);
+}
+
+int main()
+{   
+    int n;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    vector<int> current;
+    cout<<"Enter elements inside array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    subsequence(arr, n, 0, current);
+}
