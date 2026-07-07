78dc36ba3ae59594e9d4998dba10de3f7e7e6866
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-07 22:04:43 +0530

    Hashmap Question

---
+++ Hashmap/Print_Duplicate.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n,low=INT_MAX;
+    bool found=false;
+    cout<<"Enter size of the array : ";
+    cin>>n;
+    int arr[n];
+    unordered_map <int,int> mp;
+    cout<<"Enter elements inside the array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // Inserting array inside Hashmap
+    for(int i=0;i<n;i++){
+        mp[arr[i]]++;
+    }
+    cout<<"Duplicate elements are : ";
+    for(auto x : mp){
+        if(x.second>1){
+            found=true;
+            cout<<x.first<<" ";
+        }
+    }
+    if(!found){
+        cout<<"Not exist : ";
+    }
+    cout<<endl;
+    return 0;
+}
