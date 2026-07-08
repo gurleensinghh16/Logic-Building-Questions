5da34902b1f3bf5dcdd60dae95810fcf5c8cf00c
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-08 22:37:59 +0530

    Hashmap Question

--- Hashmap/Mising_Nos.cpp
+++ Hashmap/Mising_Nos.cpp
@@ -7,20 +7,31 @@ int main ()
     cout<<"Enter size of array : ";
     cin>>n;
     int arr[n];
+    vector <int> ans;
     unordered_map <int,int> mp;
     cout<<"Enter elements : ";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
+    int max=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    // Inserting in Hashmap
     for(int i=0;i<n;i++){
         mp[arr[i]]++;
     }
-    // Finding No.
-    for(int i=1;i<=n;i++){
+    // Inserting inside vector
+    for(int i=1;i<=max;i++){
         if(mp[i]==0){
-            cout<<"The missing Number is : "<<i<<endl;
-            break;
+            ans.push_back(i);
         }
     }
+    for(auto x : ans){
+        cout<<x<<" ";
+    }
+    cout<<endl;
     return 0;
 }
