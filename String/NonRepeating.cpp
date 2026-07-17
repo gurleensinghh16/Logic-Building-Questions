eb314f70e0a515eacd85eef786cc4c7a1bea665b
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-17 14:06:58 +0530

    String Completed

--- String/NonRepeating.cpp
+++ String/NonRepeating.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main ()
 {
     string str,ans;
+    int ans2;
     cout<<"Enter a sentence :";
     getline(cin,str);
     for(int i=0;i<str.size();i++){
@@ -10,15 +11,17 @@ int main ()
         for(int j=0;j<str.size();j++){
             if(str[i]==str[j]){
                 count+=1;
+                ans2=i;
             }
         }
         if(count==1){
             ans+=str[i];
+            break;
         }
     }
     for(auto x: str){
         if(ans.find(x)!=string :: npos){
-            cout<<"The first Non repeating char is : "<<x<<endl;
+            cout<<"The first Non repeating char is : "<<x<<endl<<" at index : "<<ans2<<endl;
             break;
         }
     }
