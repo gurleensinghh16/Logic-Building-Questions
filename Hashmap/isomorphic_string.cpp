954de561253e843fb85ccb96ce1055e4d141b80a
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-07-28 12:20:01 +0530

    Hashing Question

--- Hashmap/isomorphic_string.cpp
+++ Hashmap/isomorphic_string.cpp
@@ -13,8 +13,26 @@ int main ()
     for(int i=0;i<str1.size();i++){
         mp1[str1[i]]++;
     }
-    for(int i=0;i<str1.size();i++){
+    for(int i=0;i<str2.size();i++){
         mp2[str2[i]]++;
     }
+    vector <int> vec1;
+    vector <int> vec2;
+    //inserting hashmap count in arr :
+    for(auto x : mp1){
+        vec1.push_back(x.second);
+    }
+    for(auto x : mp2){
+        vec2.push_back(x.second);
+    }
+    sort(vec1.begin(),vec1.end());
+    sort(vec2.begin(),vec2.end());
+    if(vec1==vec2){
+        cout<<"True : ";
+    }
+    else{
+        cout<<"False : ";
+    }
+    cout<<endl;
     return 0;
 }
