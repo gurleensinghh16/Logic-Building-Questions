91293971f1cc0c429552bf4286d6cf457ff5ed16
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-08-01 14:05:15 +0530

    TwoPointer Question

---
+++ TwoPointer/Move_Zero.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements inside array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // Moving Zeroes :
+    int i=0,j=0;
+    while(i<n){
+        if(arr[j]!=0){
+            j++;
+        }
+        else if(i>j and arr[i]!=0){
+            swap(arr[i],arr[j]);
+            i++;
+            j++;
+        }
+        else{
+            i++;
+        }
+    }
+    cout<<"Array after removing zero is : ";
+    for(auto x : arr){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
