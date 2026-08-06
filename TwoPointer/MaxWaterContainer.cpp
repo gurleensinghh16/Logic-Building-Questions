e22ccd06897480d55fd2d3b128b77c6aa9c71b5e
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-08-06 14:11:14 +0530

    TwoPointer Question

---
+++ TwoPointer/MaxWaterContainer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"No. of containers : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter heights of containers : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // Finding container :
+    int prevArea=0,area,width,maxArea=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            width=j-i;
+            area=min(arr[i],arr[j])*width;
+            maxArea=max(area,maxArea);
+        }
+    }
+    cout<<"Maximum area is : "<<maxArea<<endl;
+    return 0;
+}
