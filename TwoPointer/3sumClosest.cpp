94fdc5d40d7489a093301c1def1a26415238d2a8
gurleensinghh16 <gurleensinghh16@gmail.com>
2026-08-08 21:00:48 +0530

    TwoPointer Question

--- TwoPointer/3sumClosest.cpp
+++ TwoPointer/3sumClosest.cpp
@@ -4,7 +4,7 @@ int main ()
 {
     int n,target;
     bool found=false;
-    cout<<"Enter size of array : ";
+    cout<<"Enter Size of  array : ";
     cin>>n;
     int arr[n];
     cout<<"Enter elements inside array : ";
@@ -14,6 +14,7 @@ int main ()
     cout<<"Enter target Element : ";
     cin>>target;
     // Finding 3 sum :
+    int closest=INT_MAX,diff=0,sum=0,finalsum=0;
     for(int i=0;i<n-2;i++){
         for(int j=i+1;j<n-1;j++){
             for(int k=j+1;k<n;k++){
@@ -22,13 +23,26 @@ int main ()
                 cout<<"Element found at index : "<<i<<" "<<j<<" "<<k;
                 break;
                 }
+                else{
+                    sum=arr[i]+arr[j]+arr[k];
+                    if(sum>0){
+                        diff=sum-target;
+                    }
+                    if(sum<0){
+                        diff=-(sum-target);
+                    }
+                    if(diff<closest){
+                        finalsum=sum;
+                    }
+                    closest=min(closest,diff);
+                }
             }
             if(found) break;
         }
         if(found) break;
     }
     if(!found){
-        cout<<"Target Doesnot exist : ";
+        cout<<"Closest sum is : "<<finalsum;
     }
     cout<<endl;
     return 0;
